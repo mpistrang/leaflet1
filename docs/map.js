910c21410ece7d2f18449ac5fbae8aa7bbb90e93
Michael Pistrang <michael@pistrang.com>
2021-05-22 07:37:17 -0400

    layer controls

--- docs/map.js
+++ docs/map.js
@@ -1,22 +1,46 @@
 // Set up the map
-const map = L.map("map", { center: [30.0, 0.0], zoom: 3 });
 
 /*
 Add the base layer from mapbox to the map
 */
-L.tileLayer(
-  // default public token
-  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWljaGFlbHBpc3RyYW5nIiwiYSI6ImNrb3ozOWV5NTBncXEydmxxdmc5dWhlNGEifQ.ZN7YsaG1SRwuaZ_GH00Zvg",
-  {
-    maxZoom: 18,
-    attribution:
-      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: "mapbox/light-v9",
-    tileSize: 512,
-    zoomOffset: -1,
-  }
-).addTo(map);
+
+const mbAttr =
+    'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  mbUrl =
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWljaGFlbHBpc3RyYW5nIiwiYSI6ImNrb3ozOWV5NTBncXEydmxxdmc5dWhlNGEifQ.ZN7YsaG1SRwuaZ_GH00Zvg";
+
+const grayscale = L.tileLayer(mbUrl, {
+  id: "mapbox/light-v9",
+  tileSize: 512,
+  zoomOffset: -1,
+  attribution: mbAttr,
+});
+const streets = L.tileLayer(mbUrl, {
+  id: "mapbox/streets-v11",
+  tileSize: 512,
+  zoomOffset: -1,
+  attribution: mbAttr,
+});
+
+const points = L.layerGroup();
+
+const map = L.map("map", {
+  center: [30.0, 0.0],
+  zoom: 3,
+  layers: [grayscale, points],
+});
+
+const baseLayers = {
+  Grayscale: grayscale,
+  Streets: streets,
+};
+
+const overlays = {
+  Points: points,
+};
+
+L.control.layers(baseLayers, overlays).addTo(map);
 
 /*
 Set up the popup for each feature based onthe geojson propeties
@@ -39,7 +63,7 @@ const pointsStyle = {
 };
 
 /*
-Load the geojson from the file and add it to the map
+Load the geojson from the file and add it to the points layer
 */
 $.getJSON("points.geojson", function (data) {
   const geojsonLayer = L.geoJSON(data, {
@@ -48,5 +72,5 @@ $.getJSON("points.geojson", function (data) {
       return L.circleMarker(latlng, pointsStyle);
     },
   });
-  geojsonLayer.addTo(map);
+  geojsonLayer.addTo(points);
 });
