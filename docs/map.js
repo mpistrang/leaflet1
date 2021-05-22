e37c96c1419a731d367a818b98d36dc15571e7ac
Michael Pistrang <michael@pistrang.com>
2021-05-22 07:08:53 -0400

    and the map

---
+++ docs/map.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+  $.ajaxSetup({ cache: false });
+});
+
+var map = L.map("map", { center: [30.0, 0.0], zoom: 3 });
+
+L.tileLayer(
+  // default public token
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWljaGFlbHBpc3RyYW5nIiwiYSI6ImNrb3ozOWV5NTBncXEydmxxdmc5dWhlNGEifQ.ZN7YsaG1SRwuaZ_GH00Zvg",
+  {
+    maxZoom: 18,
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: "mapbox/light-v9",
+    tileSize: 512,
+    zoomOffset: -1,
+  }
+).addTo(map);
+
+var baseballIcon = L.icon({
+  iconUrl: "baseball-marker.png",
+  iconSize: [32, 37],
+  iconAnchor: [16, 37],
+  popupAnchor: [0, -28],
+});
+
+function onEachFeature(feature, layer) {
+  if (feature.properties && feature.properties.popupContent) {
+    const popupContent = feature.properties.popupContent;
+    layer.bindPopup(popupContent);
+  }
+}
+
+var pointsStyle = {
+  radius: 3,
+  fillColor: "#ff7800",
+  color: "#000",
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 0.8,
+};
+
+$.getJSON("points.geojson", function (data) {
+  var geojsonLayer = L.geoJSON(data, {
+    onEachFeature: onEachFeature,
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng, pointsStyle);
+    },
+  });
+  geojsonLayer.addTo(map);
+});
